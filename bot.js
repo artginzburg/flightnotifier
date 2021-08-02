9797be011c375191c27419bc47b13fbeb9f16023
artginzburg <art.ginzburg@ya.ru>
2021-08-03 02:03:42 +0300

    move translayoutTypo to /modules

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf');
-const ru = require('convert-layout/ru');
-const cyrillicToTranslit = require('cyrillic-to-translit-js')();
+
+const translayoutTypo = require('./modules/telegraf-translayout-typo');
 
 const setupAdmin = require('./commands/setupAdmin');
 
@@ -25,45 +25,7 @@ bot.command(['start', 'help'], (ctx) => {
   );
 });
 
-bot.on('text', (ctx, next) => {
-  // Terms:
-  // layout-converted message — (e.g. `рудз` => `help`)
-  // transliterated message — (e.g. `админ` => `admin`)
-  const { chat, message } = ctx;
-
-  if (chat.type !== 'private') {
-    // bypass if chat is not private
-    return next();
-  }
-
-  if (message.entities && message.entities.some((entity) => entity.type === 'bot_command')) {
-    // bypass if the message is syntactically valid as a command (Telegram's built-in validation)
-    return next();
-  }
-
-  if (!['/', '.', '?', '÷'].includes(message.text[0])) {
-    // bypass if the message wasn't intended to be a command
-    return next();
-  }
-
-  if (message.text[0] === '/') {
-    const translitConverted = cyrillicToTranslit.transform(message.text);
-    // reply with transliterated message if the initial message was surely a command
-    return ctx.reply(`${translitConverted}?`);
-  }
-
-  // layout-convert the initial message
-  const layoutConverted = ru.toEn(message.text);
-
-  if (layoutConverted === message.text) {
-    // bypass if layout-converted message equals the initial message
-    return next();
-  }
-
-  // reply with layout-converted message
-  return ctx.reply(`${layoutConverted}?`);
-  // TODO: implement checking bot.telegram.getMyCommands() for including the converted layout text. Should be turned on by default and controlled with the package constructor
-});
+bot.use(translayoutTypo);
 
 bot.launch();
 
---
+++ modules/telegraf-translayout-typo.js
@@ -0,0 +1,42 @@
+const ru = require('convert-layout/ru');
+const cyrillicToTranslit = require('cyrillic-to-translit-js')();
+
+module.exports = function translayoutTypo(ctx, next) {
+  // Terms:
+  // layout-converted message — (e.g. `рудз` => `help`)
+  // transliterated message — (e.g. `админ` => `admin`)
+  const { chat, message } = ctx;
+
+  if (chat.type !== 'private') {
+    // bypass if chat is not private
+    return next();
+  }
+
+  if (message.entities && message.entities.some((entity) => entity.type === 'bot_command')) {
+    // bypass if the message is syntactically valid as a command (Telegram's built-in validation)
+    return next();
+  }
+
+  if (!['/', '.', '?', '÷'].includes(message.text[0])) {
+    // bypass if the message wasn't intended to be a command
+    return next();
+  }
+
+  if (message.text[0] === '/') {
+    const translitConverted = cyrillicToTranslit.transform(message.text);
+    // reply with transliterated message if the initial message was surely a command
+    return ctx.reply(`${translitConverted}?`);
+  }
+
+  // layout-convert the initial message
+  const layoutConverted = ru.toEn(message.text);
+
+  if (layoutConverted === message.text) {
+    // bypass if layout-converted message equals the initial message
+    return next();
+  }
+
+  // reply with layout-converted message
+  return ctx.reply(`${layoutConverted}?`);
+  // TODO: implement checking bot.telegram.getMyCommands() for including the converted layout text. Should be turned on by default and controlled with the package constructor
+};
