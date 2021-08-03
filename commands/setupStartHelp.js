e9e99b256d52275b2c9c573bc287cc1165d5a280
artginzburg <art.ginzburg@ya.ru>
2021-08-03 21:37:57 +0300

    start/help command now works faster

--- commands/setupStartHelp.js
+++ commands/setupStartHelp.js
@@ -3,12 +3,7 @@ const pkg = require('../package.json');
 const { findUser } = require('../models');
 
 module.exports = function setupStartHelp(bot) {
-  bot.command(['start', 'help'], async (ctx) => {
-    let user = await findUser(ctx.message.from.id);
-    if (!user.startedUsing) {
-      user.startedUsing = true;
-      user = await user.save();
-    }
+  bot.command(['start', 'help'], (ctx) => {
     ctx.replyWithMarkdown(
       `Hi. I'm ${bot.botInfo.first_name} open-source bot from ${
         pkg.repository
@@ -16,5 +11,12 @@ module.exports = function setupStartHelp(bot) {
         pkg.description || `...oh. Sorry, still in development`
       }.\n\nCry for /help or read the commands list. Might be helpful.`
     );
+
+    findUser(ctx.message.from.id).then((user) => {
+      if (!user.startedUsing) {
+        user.startedUsing = true;
+        user.save();
+      }
+    });
   });
 };
