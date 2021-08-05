40d66a6649f0d6479bd6fe804986654edcd5d638
artginzburg <art.ginzburg@ya.ru>
2021-08-05 20:51:39 +0300

    add prepare script

--- models/index.js
+++ models/index.js
@@ -5,6 +5,9 @@ const { MONGO } = process.env;
 if (!MONGO) {
   throw new Error('MONGO env variable must be provided!');
 }
-mongoose.connect(MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(MONGO, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = require('./user');
---
+++ prepare/env.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+const currentRelativePath = '../';
+
+const envPath = currentRelativePath + '.env';
+
+const envSchemaPath = currentRelativePath + '.env.schema';
+const envDefaultsPath = currentRelativePath + '.env.defaults';
+
+async function upsertFile(name, dataIfNotFound) {
+  const nameFromDirname = `${__dirname}/${name}`;
+
+  fs.writeFile(nameFromDirname, dataIfNotFound, { flag: 'wx' }, (err) => {
+    if (err) {
+      return;
+    }
+    console.log(`Prepared ${name.substring(currentRelativePath.length)} file`);
+  });
+}
+
+function envParse(path) {
+  const env = fs.readFileSync(`${__dirname}/${path}`, { encoding: 'utf-8' }).trim();
+  const envArr = env.split('\n');
+  let envObject = {};
+  envArr.forEach((el) => {
+    const elSplitted = el.split('=');
+    const key = elSplitted[0];
+    const value = elSplitted[1] ?? '';
+    envObject[key] = value;
+  });
+  return envObject;
+}
+
+function envStringifyInit(envObj) {
+  let string = '';
+
+  for (const key in envObj) {
+    if (envObj[key]) {
+      string += '# ';
+    }
+
+    string += `${key}=\n`;
+  }
+
+  return string.trim();
+}
+
+const envSchema = envParse(envSchemaPath);
+const envDefaults = envParse(envDefaultsPath);
+
+const stringifiedEnv = envStringifyInit({ ...envSchema, ...envDefaults });
+
+upsertFile(envPath, stringifiedEnv);
