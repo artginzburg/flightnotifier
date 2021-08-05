64bc0fc52013d30068df997968c2bcc54820d4dd
artginzburg <art.ginzburg@ya.ru>
2021-08-05 12:24:04 +0300

    BOT_OWNER_ID done right

--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -1,12 +1,12 @@
 const { findUser } = require('../models');
 
-const { BOT_OWNER_ID } = process.env;
+const BOT_OWNER_ID = Number(process.env.BOT_OWNER_ID);
 
 function checkRole(roleEnum) {
   return async (ctx, next) => {
     const user = await findUser(ctx.from.id);
 
-    if (user.role >= roleEnum || user._id === Number(BOT_OWNER_ID)) {
+    if (user.role >= roleEnum || user._id === BOT_OWNER_ID) {
       return next();
     }
     return Promise.resolve();
